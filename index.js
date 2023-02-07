a9071d5a68d6397ba06951d7cf17500dcb37e708
xoz5 <113217028+xoz5@users.noreply.github.com>
2023-02-07 08:19:46 -0800

    Add math operator function

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
     return number1 / number2;
 }
+
+function operate(operator, number1, number2) {
+    switch(operator) {
+        case '-':
+            return subtract(number1, number2);
+        case '*':
+            return multiply(number1, number2);
+        case '/':
+            return divide(number1, number2);
+        case '+':
+            return add(number1, number2);
+    }
+}
