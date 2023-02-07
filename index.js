d4a5c3d9b9e87cc07dfea5acc9439e57fad1cc30
xoz5 <113217028+xoz5@users.noreply.github.com>
2023-02-07 07:53:54 -0800

    Add math operation functions

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
